98c56f57eb30d72a79a114bf218573cd1fa7b06c
TheSrinathKamath <82434922+Srinath-Kamath@users.noreply.github.com>
2021-06-19 18:28:57 +0530

    update to service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,10 +1,2 @@
-const initSW = () => {
-    navigator.serviceWorker &&
-        navigator.serviceWorker
-            .register('./service-worker.js')
-            .then((registration) => {
-            });
-};
-
 self.addEventListener('fetch', (event) => {
 });
